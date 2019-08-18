8c6cc131d80c08a734212d7837c1f3bb49639919
Abdul Kareem <bitguiders@gmail.com>
2019-08-18 17:52:12 -0400

    Initital project setup

---
+++ src/main/java/bpi4jt_01/bpi4jt_01/App.java
@@ -0,0 +1,13 @@
+package bpi4jt_01.bpi4jt_01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
